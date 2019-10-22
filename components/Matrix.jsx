ca97302fad1cc380c5462a59ce0e9edd6776e38f
ddmgarv <garv1998131@gmail.com>
2019-10-21 22:39:20 -0300

    Final result

--- components/Matrix.jsx
+++ components/Matrix.jsx
@@ -3,37 +3,53 @@ import React from "react";
 const Matrix = ({ matrix }) => {
   const countOIE = () => {
     console.log(matrix);
-    for(let y = 0; y < matrix.length; y++) {
-      for(let x = 0; x < matrix[y].length; x++) {
-        if(matrix[y][x] == "O"){
-          if((matrix[y][x + 1] == "I") && (matrix[y][x + 2] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y + 1][x + 1] == "I") && (matrix[y + 2][x + 2] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y + 1][x] == "I") && (matrix[y + 2][x] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y + 1][x - 1] == "I") && (matrix[y + 2][x - 2] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y][x - 1] == "I") && (matrix[y][x - 2] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y - 1][x - 1] == "I") && (matrix[y - 2][x - 2] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y - 1][x] == "I") && (matrix[y - 2][x] == "E")){
-            console.log("found something");
-          }
-          if((matrix[y - 1][x + 1] == "I") && (matrix[y - 2][x + 2] == "E")){
-            console.log("found something");
-          }
+    for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length; x++) {
+        if (matrix[y][x] == "O") {
+          try {
+            if (matrix[y][x + 1] == "I" && matrix[y][x + 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y + 1][x + 1] == "I" && matrix[y + 2][x + 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y + 1][x] == "I" && matrix[y + 2][x] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y + 1][x - 1] == "I" && matrix[y + 2][x - 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y][x - 1] == "I" && matrix[y][x - 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y - 1][x - 1] == "I" && matrix[y - 2][x - 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y - 1][x] == "I" && matrix[y - 2][x] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
+          try {
+            if (matrix[y - 1][x + 1] == "I" && matrix[y - 2][x + 2] == "E") {
+              console.log("found something");
+            }
+          } catch (error) {}
         }
       }
     }
-  }
+  };
   return (
     <div className="matrix">
       {matrix.map(item => (
